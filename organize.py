a1add250319bf8c698a4a4fea85342d04e483445
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2024-06-08 10:54:59 +0530

    Improved working without the need for preexisting folders

--- organize.py
+++ organize.py
@@ -1,83 +1,87 @@
 import os
-import sys
 import glob
 import shutil
 
+def create_directory(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 def main():
+    # store the path of downloads folder
+    downloads_folder = "/home/navadeep/Downloads/"
 
-    #store the path of downloads folder
-    downloads_folder = "/home/navadeep-reddy/Downloads/"
+    # create necessary folders
+    folders = ["Photos", "Video", "Docs", "Audio", "Compressed", "Code", "Misc", "Folders"]
+    for folder in folders:
+        create_directory(os.path.join(downloads_folder, folder))
 
-    #store the names of all the image files in a variable
+    # store the names of all the image files in a variable
     images_names = []
     image_types = ["*.jpg", "*.png", "*.svg", "*.gif", "*.webp"]
     for temp in image_types:
         images_names.extend(glob.glob(os.path.join(downloads_folder, temp))) 
 
-    #move images to Pictures folder
+    # move images to Photos folder
     for name in images_names:
-        shutil.move(name, "/home/navadeep-reddy/Downloads/Photos")
+        shutil.move(name, os.path.join(downloads_folder, "Photos"))
 
-
-    #store the names of all videos in a variable
+    # store the names of all videos in a variable
     video_names = []
     video_types = ["*.mp4", "*.mov", "*.webm", "*.avi", "*.mkv"]
     for temp in video_types:
         video_names.extend(glob.glob(os.path.join(downloads_folder, temp)))
 
     for name in video_names:
-        shutil.move(name, "/home/navadeep-reddy/Downloads/Video")
+        shutil.move(name, os.path.join(downloads_folder, "Video"))
 
-
-    #store all the names of documents
+    # store all the names of documents
     doc_names = []
     doc_types = ["*.pptx", "*.docx", "*.pdf", "*.doc", "*.xlsx", "*.odt"] 
     for temp in doc_types:
         doc_names.extend(glob.glob(os.path.join(downloads_folder, temp)))
 
     for name in doc_names:
-        shutil.move(name, "/home/navadeep-reddy/Downloads/Docs")
+        shutil.move(name, os.path.join(downloads_folder, "Docs"))
 
-    #store all the names of audio files
+    # store all the names of audio files
     au_names = []
     au_types = ["*.mp3", "*.m4a", "*.wav", "*.ogg", "*.opus"]
     for temp in au_types:
         au_names.extend(glob.glob(os.path.join(downloads_folder, temp)))
 
     for name in au_names:
-        shutil.move(name, "/home/navadeep-reddy/Downloads/Audio")
+        shutil.move(name, os.path.join(downloads_folder, "Audio"))
 
-    #store compressed file name
-    comp_name = []
+    # store compressed file names
+    comp_names = []
     comp_types = ["*.zip", "*.rar", "*.tar", "*.z"]
     for temp in comp_types:
-        comp_name.extend(glob.glob(os.path.join(downloads_folder, temp)))
-    for name in comp_name:
-        shutil.move(name, "/home/navadeep-reddy/Downloads/Compressed")
+        comp_names.extend(glob.glob(os.path.join(downloads_folder, temp)))
+    for name in comp_names:
+        shutil.move(name, os.path.join(downloads_folder, "Compressed"))
 
-    #store names of codes
-    c_name = []
+    # store names of code files
+    c_names = []
     c_types = ["*.py", "*.cpp", "*.html", "*.js", "*.css", "*.c"]
     for temp in c_types:
-        c_name.extend(glob.glob(os.path.join(downloads_folder, temp)))
-    for name in c_name:
-        shutil.move(name, "/home/navadeep-reddy/Downloads/Code")
+        c_names.extend(glob.glob(os.path.join(downloads_folder, temp)))
+    for name in c_names:
+        shutil.move(name, os.path.join(downloads_folder, "Code"))
 
-    #move the remaining to misc
+    # move the remaining files to Misc
     misc_names = glob.glob(os.path.join(downloads_folder, "*.*"))
     for name in misc_names:
-        shutil.move(name, "/home/navadeep-reddy/Downloads/Misc")
+        shutil.move(name, os.path.join(downloads_folder, "Misc"))
 
-    #get the names of all the folders
+    # get the names of all the folders
     fa_names = glob.glob(os.path.join(downloads_folder, "*"))
     f_names = []
     for temp in fa_names:
-        if temp not in ["/home/navadeep-reddy/Downloads/Code", "/home/navadeep-reddy/Downloads/Misc", "/home/navadeep-reddy/Downloads/Compressed", "/home/navadeep-reddy/Downloads/Audio", "/home/navadeep-reddy/Downloads/Video", "/home/navadeep-reddy/Downloads/Docs", "/home/navadeep-reddy/Downloads/Photos"]:
+        if temp not in [os.path.join(downloads_folder, folder) for folder in folders]:
             f_names.append(temp)
 
     for name in f_names:
-        shutil.move(name, "/home/navadeep-reddy/Downloads/Folders")
-
+        shutil.move(name, os.path.join(downloads_folder, "Folders"))
 
 main()
+
